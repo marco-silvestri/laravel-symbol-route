76bd1c7b66831d5eeb608bbbe75954b1b35f7939
marco-silvestri <m.silvestri03@gmail.com>
2020-06-11 09:38:20 +0200

    Basic styling with Bootstrap

--- resources/views/shared/layout.blade.php
+++ resources/views/shared/layout.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-</head>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
 <body>
     <header>
         @include('shared.header')
